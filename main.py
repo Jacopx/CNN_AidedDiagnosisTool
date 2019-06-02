c32118adecfbb9424920381d096271ca639f2568
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-06-02 08:29:18 +0200

    Creating main file and adding .gitignore for Python and PyCharm

---
+++ main.py
@@ -0,0 +1,12 @@
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+# *                          CNN_AidedDiagnosisTool                         *
+# *            https://github.com/Jacopx/CNN_AidedDiagnosisTool             *
+# * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+
+def main():
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()

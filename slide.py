a0f25c2226671939473c8d5b4d3e1c64aa21186c
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-06-05 18:01:03 +0200

    Starting sample division

--- slide.py
+++ slide.py
@@ -4,9 +4,16 @@
 # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 import openslide
 
+SAMPLE_SIZE = 64
+
 
 def slide_info(slide_path):
     slide = openslide.open_slide(slide_path)
     width = int(slide.dimensions[0])
     height = int(slide.dimensions[1])
-    print(slide.properties)
+    # print(slide.properties)
+    w_sample = width // SAMPLE_SIZE + 1
+    h_sample = height // SAMPLE_SIZE + 1
+    print(slide_path, end=" ==> ")
+    print(width, height, end=" / ")
+    print(w_sample, h_sample)

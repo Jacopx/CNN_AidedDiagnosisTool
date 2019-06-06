bec2456e3755c0e97d2de99b79fa50aeccc457b6
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-06-06 11:30:43 +0200

    Creating crop function and removing not working overlapping

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ DATASET_FOLDER = "dataset"
 
 def main():
     print("Opening Whole-slide")
+
+    # Production
     # for filename in os.listdir(DATASET_FOLDER):
     #     if filename.endswith(".svs"):
     #         slide.slide_info(DATASET_FOLDER + "/" + filename)
+
+    # Debug
     slide.slide_info(DATASET_FOLDER + "/2_AC_1.svs")
+    slide.crop(DATASET_FOLDER + "/2_AC_1.svs")
 
 
 if __name__ == "__main__":
--- slide.py
+++ slide.py
@@ -4,7 +4,8 @@
 # * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 import openslide
 
-SAMPLE_SIZE = 256
+SS = 256  # Sample Size
+LVL = 0   # Level of dimensionality
 CROP_FOLDER = 'crop'
 
 
@@ -13,30 +14,53 @@ def slide_info(slide_path):
     width = int(slide.dimensions[0])
     height = int(slide.dimensions[1])
     # print(slide.properties)
-
-    # Computing overlapping for width
-    w_sample = width / SAMPLE_SIZE
-    dec_w = w_sample % 1
-    over_w = dec_w * SAMPLE_SIZE
-
-    # Computing overlapping for height
-    h_sample = height / SAMPLE_SIZE
-    dec_h = h_sample % 1
-    over_h = dec_h * SAMPLE_SIZE
+    # print(slide.level_dimensions)
 
     print(slide_path, end=" ==> ")
     print(width, height, end=" / ")
-    print(w_sample, h_sample, end=" :: ")
+
+    # Computing overlapping for width
+    w_sample = width / SS
+    dec_w = w_sample % 1
+    over_w = dec_w * SS
+
+    # Computing overlapping for height
+    h_sample = height / SS
+    dec_h = h_sample % 1
+    over_h = dec_h * SS
+
+    print(str(w_sample) + "::" + str(h_sample), end=" || ")
     print(over_w, over_h)
 
+
+def crop(slide_path):
+    slide = openslide.open_slide(slide_path)
+    width = int(slide.dimensions[0])
+    height = int(slide.dimensions[1])
+
+    # Computing overlapping for width
+    w_sample = width / SS
+    # dec_w = w_sample % 1
+    # over_w = dec_w * SS  #NOT GOOD
+
+    # Computing overlapping for height
+    h_sample = height / SS
+    # dec_h = h_sample % 1
+    # over_h = dec_h * SS  #NOT GOOD
+
     # The first cycle is for production, the second is for debug
+
     # for shift_w in range(0, int(w_sample)):
     #     for shift_h in range(0, int(h_sample)):
     for shift_w in range(0, 10):
         for shift_h in range(0, 10):
-            crop = slide.read_region((SAMPLE_SIZE*shift_w, SAMPLE_SIZE*shift_h), 2, (SAMPLE_SIZE, SAMPLE_SIZE))
+            # @TODO: Managing overlapping decisions
+            position = (SS * shift_w, SS * shift_h)
+            print(position, end="\t=\t")
+            crop_region = slide.read_region(position, LVL, (SS, SS))
+
             crop_name = CROP_FOLDER + '/crop_' + str(shift_w) + 'x' + str(shift_h) + '.png'
-            crop.save(crop_name)
-            print("Generating: " + 'crop_' + str(shift_w) + 'x' + str(shift_h))
+            crop_region.save(crop_name)
+            print('crop_' + str(shift_w) + 'x' + str(shift_h))
 
     print("\n # Crop: " + str(int(w_sample*h_sample)))
